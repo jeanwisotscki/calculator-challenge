2fbd19a863e18e3cb88cd0f2925d913f65e83197
Jean Wisotscki <jean1wisotscki@gmail.com>
2021-12-29 18:07:35 -0300

    script em progresso

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 const billValue = document.querySelector('#bill-input')
 const tipValue = document.querySelector('#tip-value')
 const peopleInput = document.querySelector('#people-input')
+const tipButton = document.querySelectorAll('#select-tip button')
+const resetButton = document.querySelector('#reset-btn')
 
 
+function selectTipButton() {
 
-let pessoas
-function funcao(valor) {
-    pessoas = valor
-    return pessoas
+    function toggleActiveClass(button) {
+        tipButton.forEach((button) => {
+            button.classList.remove('active')
+        })
+
+        button.classList.add('active')
+
+        console.log(button.value)
+    }
+
+
+    tipButton.forEach((button) => {
+        button.addEventListener('click', () => {
+            toggleActiveClass(button)
+        })
+    })
 }
+selectTipButton()
 
-peopleInput.addEventListener('keyup', () => {
-    funcao(peopleInput.value)
+
+
+
+
+
+
+
+resetButton.addEventListener('click', () => {
+    const soma = document.querySelector('#total-value')
+    soma.innerHTML = '$' + ((billValue.value * 0.15) / peopleInput.value).toFixed(2)
 })
 
 
 
 
-billValue.addEventListener('keyup', () => {
-    const teste = billValue.value
-    tipValue.innerHTML = '$' + ((teste * 0.15) / pessoas).toFixed(2)
 
-})
+
+
+
+
+
+
+
