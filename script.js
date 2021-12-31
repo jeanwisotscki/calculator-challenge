c28681ba7211c0a99e0eebbc652a04685eb61933
Jean Wisotscki <jean1wisotscki@gmail.com>
2021-12-31 15:35:20 -0300

    script refeito: funções toggleActiveClass() e calculate() adicionadas

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-const billValue = document.querySelector('#bill-input')
-const tipValue = document.querySelector('#tip-value')
+const billInput = document.querySelector('#bill-input')
 const peopleInput = document.querySelector('#people-input')
+const tipResult = document.querySelector('#tip-value')
+const totalResult = document.querySelector('#total-value')
 const tipButton = document.querySelectorAll('#select-tip button')
 const resetButton = document.querySelector('#reset-btn')
+let tipPercentage
 
 
+function toggleActiveClass(button) {
+    tipButton.forEach((btn) => {
+        btn.classList.remove('active')
+    })
+
+    button.classList.add('active')
+    tipPercentage = button.value
+}
+
+function calculate() {
+    resetButton.onclick = () => {
+
+        tipPercentage = Number(tipPercentage)
+        const bill = Number(billInput.value)
+        const numberOfPeople = Number(peopleInput.value)
+        const tipAmount = Number((bill * tipPercentage) / numberOfPeople)
+        const total = Number(tipAmount + (bill / numberOfPeople))
+
+        tipResult.innerHTML = '$' + tipAmount.toFixed(2)
+        totalResult.innerHTML = '$' + total.toFixed(2)
+
+        console.log(typeof bill)
+        console.log(typeof numberOfPeople)
+        console.log(typeof tipPercentage)
+        console.log(typeof tipAmount)
+        console.log(typeof total)
+    }
+}
+
+tipButton.forEach((btn) => {
+    btn.addEventListener('click', () => {
+
+        toggleActiveClass(btn)
+        calculate()
+    })
+})
