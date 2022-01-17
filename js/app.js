135487628050d357525155346d306e68c874086f
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-01-17 19:28:31 -0300

    rgx de validação ok

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 const bill = document.querySelector('#inp-bill')
-let billValue = 0 // default
+const people = document.querySelector('#inp-people')
+
+let peopleValue = 1 // default
+
 
 bill.addEventListener('input', setBillValue)
+
+let billValue = 0 // default
+
+function validateFloat(value) {
+    let rgx = /^[0-9]*\.?[0-9]*$/
+
+    return value.match(rgx)
+}
 
 function setBillValue() {
     if (bill.value.includes(',')) {
         bill.value = bill.value.replace(',', '.')
     }
 
+    if (!validateFloat(bill.value)) {
+        bill.value = bill.value.substring(0, bill.value.length - 1)
+    }
+
     billValue = Number(bill.value)
-    console.log(billValue)
 }
