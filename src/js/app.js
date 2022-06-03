259eef05403b55fef907aa07fe3be7ae73e222cc
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-06-03 10:41:19 -0300

    vilidação de inputs OK

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,36 @@
 const billInput = document.querySelector("#bill");
 const tipsOptions = document.querySelectorAll(".tips .tip");
+const customTip = document.querySelector(".custom-tip");
 const numberOfPeopleInput = document.querySelector("#number-people");
 
 console.log(billInput);
 
-tipsOptions.forEach((tip) => {
+// adiciona a classe 'active' à opção de gorjeta selecionada
+function handleActiveTip(tip) {
   tip.addEventListener("click", () => {
     tipsOptions.forEach((tip) => {
       tip.classList.remove("active");
       tip.children[0].classList.remove("active");
+      customTip.value = "";
     });
     tip.classList.add("active");
     tip.children[0].classList.add("active");
   });
+}
+
+tipsOptions.forEach((tip) => {
+  handleActiveTip(tip);
 });
+
+// validação para os inputs (ativada direto no html)
+function isNumber(event) {
+  event = event ? event : window.event;
+  var charCode = event.which ? event.which : event.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    return false;
+  }
+  return true;
+}
 
 /* Event Listeners
  **************************************************/
