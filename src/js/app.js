c620006614136d240e67ed5dcf99da9f2d4b848d
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-06-03 09:59:52 -0300

    efeito focus add

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 const billInput = document.querySelector("#bill");
 const tipsOptions = document.querySelectorAll(".tips .tip");
-console.log(tipsOptions);
+const numberOfPeopleInput = document.querySelector("#number-people");
+
+console.log(billInput);
 
 tipsOptions.forEach((tip) => {
   tip.addEventListener("click", () => {
@@ -11,4 +13,25 @@ tipsOptions.forEach((tip) => {
     tip.classList.add("active");
     tip.children[0].classList.add("active");
   });
+});
+
+/* Event Listeners
+ **************************************************/
+
+// efeito focus na div do input bill
+const billInputDiv = document.querySelector(".bill-input");
+billInput.addEventListener("focusin", () => {
+  billInputDiv.classList.add("active");
+});
+billInput.addEventListener("focusout", () => {
+  billInputDiv.classList.remove("active");
+});
+
+// efeito focus na div do input number of people
+const personInputDiv = document.querySelector(".person-input");
+numberOfPeopleInput.addEventListener("focusin", () => {
+  personInputDiv.classList.add("active");
+});
+numberOfPeopleInput.addEventListener("focusout", () => {
+  personInputDiv.classList.remove("active");
 });
